eb59a8e28ef68714d8f8158a48b0165404fff6e7
Kushaldahal1 <157739541+Kushaldahal1@users.noreply.github.com>
2024-11-15 15:32:01 -0500

    Add files via upload

---
+++ exercise3.js
@@ -0,0 +1,7 @@
+const fetchData=async ()=>{
+    const re= await fetch('https://restcountries.com/v3.1/alpha/col');
+    const country = await re.json();
+    console.log(country);
+    
+    };
+    fetchData();
